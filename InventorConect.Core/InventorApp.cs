e0b708d1c8e5a5c8ccc9eb816db8ebb63b461afe
edsonlcandido <edson.luiz.candido@gmail.com>
2024-08-23 22:19:15 -0300

    Adicionar arquivos de projeto.

---
+++ InventorConect.Core/InventorApp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventor;
+
+namespace InventorConnect.Core
+{
+    public class InventorApp
+    {
+        Inventor.Application _InventorApp;
+        Inventor.Document _InventorDoc;
+        public InventorApp(Inventor.Application InvApplication)
+        {
+            _InventorApp = InvApplication;            
+        }
+
+        public class InventorPart
+        {
+
+            Inventor.Application _InventorApp;
+            Inventor.PartDocument _InventorPartDoc;
+            Inventor.Document _InventorDocument;
+            public InventorPart(Inventor.Application InvApplication, Inventor.PartDocument InvPartDocument)
+            {
+                _InventorApp = InvApplication;
+                _InventorPartDoc = InvPartDocument;
+                _InventorDocument = InvPartDocument as Inventor.Document;
+            }
+
+            public Inventor.Parameters GetParameters()
+            {
+                return _InventorPartDoc.ComponentDefinition.Parameters;
+            }
+
+        }
+
+    }
+}
---
+++ InventorConnect.Cli/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Inventor;
+using InventorConnect.Core;
+
+namespace InventorConnect.Tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventor.Application InvApp = (Inventor.Application)Marshal.GetActiveObject("Inventor.Application");
+            Inventor.PartDocument InvPartDoc = (Inventor.PartDocument)InvApp.ActiveDocument;
+            
+            InventorConnect.Core.InventorApp.InventorPart InvPart = new InventorConnect.Core.InventorApp.InventorPart(InvApp, InvPartDoc);
+
+            Inventor.Parameters InvParams = InvPart.GetParameters();
+
+            foreach (Inventor.Parameter InvParam in InvParams)
+            {
+                Console.WriteLine(InvParam.Name + " | " + InvParam.Type + " | " + InvParam.get_Units() + " | " + InvParam.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
